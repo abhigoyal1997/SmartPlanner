42d2af44ca76e7f8b350cac56de3e221c1552465
Abhinav Goyal <agabohar@gmail.com>
2018-03-04 06:15:12 +0530

    Skelton of home activity fragments

--- app/src/main/java/com/example/abhinav/smartplanner/HomeActivity.java
+++ app/src/main/java/com/example/abhinav/smartplanner/HomeActivity.java
@@ -1,41 +1,54 @@
 package com.example.abhinav.smartplanner;
 
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+            OnFragmentInteractionListener{
+
+    private DrawerLayout mDrawer = null;
+    private Toolbar mToolbar = null;
+    private FragmentManager mFManager = null;
+    private Resources mRes = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mRes = getResources();
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(mRes.getString(R.string.nav_dashboard));
+        setSupportActionBar(mToolbar);
+
+        mFManager = getSupportFragmentManager();
+        mFManager.beginTransaction().replace(R.id.flContent, DashboardFragment.newInstance()).commit();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Toast.makeText(getApplicationContext(), "Assistant to be implemented", Toast.LENGTH_SHORT).show();
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
+                this, mDrawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -44,34 +57,11 @@ public class HomeActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (mDrawer.isDrawerOpen(GravityCompat.START)) {
+            mDrawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -80,22 +70,52 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        Fragment fragment = null;
+        Class fragmentClass = null;
+        String fragmentName = null;
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        switch (id) {
+            case R.id.nav_cal:
+                fragmentClass = CalendarFragment.class;
+                fragmentName = mRes.getString(R.string.nav_cal);
+                break;
+            case R.id.nav_tasks:
+                fragmentClass = TasksFragment.class;
+                fragmentName = mRes.getString(R.string.nav_tasks);
+                break;
+            case R.id.nav_eval:
+                fragmentClass = EvalFragment.class;
+                fragmentName = mRes.getString(R.string.nav_eval);
+                break;
+            case R.id.nav_schedule:
+                fragmentClass = ScheduleFragment.class;
+                fragmentName = mRes.getString(R.string.nav_schedule);
+                break;
+            case R.id.nav_settings:
+                fragmentClass = SettingsFragment.class;
+                fragmentName = mRes.getString(R.string.nav_settings);
+                break;
+            case R.id.nav_db:
+                fragmentClass = DashboardFragment.class;
+                fragmentName = mRes.getString(R.string.nav_dashboard);
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mFManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+        mToolbar.setTitle(fragmentName);
+        item.setChecked(true);
+
+        mDrawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ app/src/main/java/com/example/abhinav/smartplanner/OnFragmentInteractionListener.java
@@ -0,0 +1,12 @@
+package com.example.abhinav.smartplanner;
+
+import android.net.Uri;
+
+/**
+ * Created by abhi on 4/3/18.
+ */
+
+public interface OnFragmentInteractionListener {
+    // TODO: Update argument type and name
+    void onFragmentInteraction(Uri uri);
+}
