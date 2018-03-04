9a2121f9b2ad8f59b79761b609851426d4070cbc
Abhinav Goyal <agabohar@gmail.com>
2018-03-04 18:25:43 +0530

    Volley handler for network requests

---
+++ app/src/main/java/com/example/abhinav/smartplanner/App.java
@@ -0,0 +1,21 @@
+package com.example.abhinav.smartplanner;
+
+import android.app.Application;
+
+/**
+ * Created by abhi on 4/3/18.
+ */
+
+public class App extends Application {
+    private static App mInstance;
+
+    public static App get() {
+        return mInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+}
