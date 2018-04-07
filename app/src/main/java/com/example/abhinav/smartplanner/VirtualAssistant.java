d08749e9fec59ea1f7b29559894209fd299ee691
Abhinav Goyal <agabohar@gmail.com>
2018-04-07 15:43:45 +0530

    init

---
+++ app/src/main/java/com/example/abhinav/smartplanner/VirtualAssistant.java
@@ -0,0 +1,39 @@
+package com.example.abhinav.smartplanner;
+
+import ai.api.AIListener;
+
+/**
+ * Created by abhi on 7/4/18.
+ */
+
+public class VirtualAssistant implements AIListener {
+    @Override
+    public void onResult(ai.api.model.AIResponse result) {
+
+    }
+
+    @Override
+    public void onError(ai.api.model.AIError error) {
+
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
+    }
+}
