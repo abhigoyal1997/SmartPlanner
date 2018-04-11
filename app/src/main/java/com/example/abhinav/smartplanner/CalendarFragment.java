d7b9e1af274a972728203490a9f89cea29dca5d5
ShraddheyaS <shendreshraddheya@gmail.com>
2018-04-12 02:37:35 +0530

    calendar recycler view

--- app/src/main/java/com/example/abhinav/smartplanner/CalendarFragment.java
+++ app/src/main/java/com/example/abhinav/smartplanner/CalendarFragment.java
@@ -3,7 +3,10 @@ package com.example.abhinav.smartplanner;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,15 +14,40 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class CalendarFragment extends Fragment {
     private OnFragmentInteractionListener mListener = null;
 
     private MaterialCalendarView calendarView = null;
+
+    DBHandler dbHandler;
+    String uid;
+
+    private RecyclerView.Adapter<ToDoTaskViewHolder> adapter;
+    private RecyclerView taskView;
+    FirestoreRecyclerOptions<ToDoTask> options;
+    List<ToDoTask> tasks;
+
+    Query query;
 
     public CalendarFragment() {
         // Required empty public constructor
@@ -52,6 +80,72 @@ public class CalendarFragment extends Fragment {
         calendarView = view.findViewById(R.id.calendar_view);
         calendarView.setSelectionMode(MaterialCalendarView.SELECTION_MODE_SINGLE);
         selectDate(CalendarDay.today());
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        uid = user != null ? user.getUid() : "";
+
+        calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                //TODO: use date here to write query
+                query = FirebaseFirestore.getInstance()
+                        .collection("users").document(uid)
+                        .collection("tasks").orderBy("date");
+                options = new FirestoreRecyclerOptions.Builder<ToDoTask>()
+                        .setQuery(query, ToDoTask.class).build();
+            }
+        });
+
+        taskView = view.findViewById(R.id.task_recycler_view);
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        taskView.setLayoutManager(linearLayoutManager);
+
+        adapter = new RecyclerView.Adapter<ToDoTaskViewHolder>() {
+            @Override
+            public void onBindViewHolder(@NonNull ToDoTaskViewHolder viewHolder, int position) {
+                ToDoTask model = tasks.get(position);
+                viewHolder.title.setText(model.getTitle());
+                Long millis = model.getTime();
+                String hm = String.format("%02d:%02d", TimeUnit.MILLISECONDS.toHours(millis),
+                        TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)), Locale.US);
+                Date date = new Date(model.getDate());
+                String[] dateParts = date.toString().split(" ");
+                String date_s = dateParts[0] + " " + dateParts[1] + " " + dateParts[2] + " " + dateParts[5];
+                viewHolder.timestamp.setText(date_s + " at " + hm);
+            }
+
+            @NonNull
+            @Override
+            public ToDoTaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.task_list_item, parent, false);
+                return new ToDoTaskViewHolder(view);
+            }
+
+            @Override
+            public int getItemCount() {
+                return tasks.size();
+            }
+        };
+
+        adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeInserted(int positionStart, int itemCount) {
+                super.onItemRangeInserted(positionStart, itemCount);
+
+                int msgCount = adapter.getItemCount();
+                int lastVisiblePosition = linearLayoutManager.findLastCompletelyVisibleItemPosition();
+
+                if (lastVisiblePosition == -1 ||
+                        (positionStart >= (msgCount - 1) &&
+                                lastVisiblePosition == (positionStart - 1))) {
+                    taskView.scrollToPosition(positionStart);
+                }
+            }
+        });
+
+        taskView.setAdapter(adapter);
+
         return view;
     }
 
